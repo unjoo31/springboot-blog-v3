8b18fd1f98a7e4703397eec7b9eb0864547a7d86
unjoo31 <unjoo31@gmail.com>
2023-07-31 11:02:04 +0900

    view 완료

---
+++ src/main/java/com/example/blogv3/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.blogv3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "user/joinForm";
+    }
+    
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "user/loginForm";
+    }
+    
+
+    @GetMapping("/updateForm")
+    public String updateForm(){
+        return "user/updateForm";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "redirect:/";
+    }
+}
